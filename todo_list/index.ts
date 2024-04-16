3a7a5494c62f7243a61f2fb6bc54648ef57daf10
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-04-16 19:38:51 +0500

    Update index.ts

--- todo_list/index.ts
+++ todo_list/index.ts
@@ -75,6 +75,3 @@ if (dropdown.menu === "view your todolist") {
   console.log(chalk.magentaBright("Task updated successfully."));
   console.log(todos);
 }
-// let index = todos.indexOf(newTask.oldtask)
-//   todos[index] = newTask.update
-//   console.log(todos);
